b7e03b41e780fb1baf3c2e7ade97889a3ba1c91a
Justin So <Justin So>
2017-04-23 10:21:30 -0400

    more stuff

--- public/createuser.php
+++ public/createuser.php
@@ -25,7 +25,7 @@
 /* Attempt MySQL server connection. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 
-$link = mysqli_connect("localhost", "cs4400_74", "e_zTUL5w", "cs4400_74");
+$link = mysqli_connect("localhost", "root", "", "cs4400_74");
  
 // Check connection
 if($link === false){
@@ -37,15 +37,50 @@ $email = $_POST['email'];
 $username = $_POST['username'];
 $password = $_POST['password'];
 $usertype = $_POST['userType'];
+$city = $_POST['city'];
+$state = $_POST['state'];
+$title = $_POST['Title'];
 
+echo $title;
+echo $city;
+echo $state; 
+
+if($usertype == "City Scientist"){
+	$sql = "INSERT INTO USER VALUES ('$email', '$username', '$password', 'City_Scientist')";
+}
+
+if($usertype == "Admin"){
+	$sql = "INSERT INTO USER VALUES ('$email', '$username', '$password', 'Admin')";
+}
+
+if($usertype == "City Official"){
+	$sql = "INSERT INTO USER VALUES ('$email', '$username', '$password', 'CITY_OFFICIAL')";
+ 	$sql = "INSERT INTO CITY_OFFICIAL VALUES ('$username', '$title', '', '$city', '$state')";
+	Echo "Fuck";
+}
 // Attempt insert query execution
-$sql = "INSERT INTO USER VALUES ('$email', '$username', '$password', '$usertype')";
+
+
 
 if(mysqli_query($link, $sql)){
+	echo $usertype;
     echo "Records inserted successfully.";
 } else{
     echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
 }
+
+$host  = $_SERVER['HTTP_HOST'];
+$uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+$admin = 'adminFunction.php';
+$official = 'cityOfficialFunction.php'
+
+if($usertype == "Admin") {
+	header("Location: http://$host$uri/$admin");
+} else if($usertype == "City Official"){
+	header("Location: http://$host$uri/$official");
+} 
+
+
  
 // Close connection
 mysqli_close($link);
