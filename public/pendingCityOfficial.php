e3831277771d0d6bc6d4bf63df9acaf006b2aef6
Justin So <Justin So>
2017-04-23 15:23:03 -0400

    more funct

--- public/pendingCityOfficial.php
+++ public/pendingCityOfficial.php
@@ -1,3 +1,47 @@
+<?php
+
+require_once 'connect.php';
+// $link = mysqli_connect("localhost", "cs4400_74", "e_zTUL5w", "cs4400_74");
+// // $link = mysqli_connect("localhost", "root", "", "cs4400_74");
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+
+$sql = 'select * from CITY_OFFICIAL where Approved is NULL';
+$dataPoints = array();
+
+if($result = mysqli_query($link, $sql)) {
+   while ($row = mysqli_fetch_array($result)) 
+   {
+      $dataPoint = array();
+      $dataPoint['username'] = $row['Username'];
+      $dataPoint['city'] = $row['City'];
+      $dataPoint['state'] = $row['State'];
+      $dataPoint['title'] = $row['Title'];
+
+      array_push($dataPoints, $dataPoint);
+
+      // array_push($location, $row['Location_Name']);
+      // array_push($type, $row['Type']);
+      // array_push($value, $row['DataValue']);
+      // array_push($timeDate, $row['DateTime']);
+   }
+}
+
+
+// print_r($location);
+// print_r($type);
+// print_r($value);
+// print_r($timeDate);
+
+mysqli_close($link);
+// print_r($citys);
+// print_r($states);
+
+?>
+
 <!DOCTYPE html>
 <html>
    <head>
@@ -8,46 +52,48 @@
    </head>
    <body>
       <div class="container">
-         <div class="row">
-            <div class="col-md-12">
-               <h2>Pending City Officials</h2>
-               <div class="table-responsive">
-                  <table id="mytable" class="table table-bordred table-striped">
-                     <thead>
-                        <th></th>
-                        <th>Username</th>
-                        <th>Email</th>
-                        <th>City</th>
-                        <th>State</th>
-                        <th>Title</th>
-                     </thead>
-                     <tbody>
-                        <tr>
-                           <td><input type="checkbox" class="checkthis" /></td>
-                           <td>Mohsin</td>
-                           <td>Irshad</td>
-                           <td>CB 106/107 Street # 11 Wah Cantt Islamabad Pakistan</td>
-                           <td>isometric.mohsin@gmail.com</td>
-                           <td>Mayor</td>
-                        </tr>
-                     </tbody>
-                  </table>
-               </div>
-            </div>
-         </div>
-         <div class="form-group">
+         <form method="post" action="updateCityOfficial.php">
             <div class="row">
-               <div class="col-sm-6 col-sm-offset-3">
-                  <input type="button" name="accept" id="accept" tabindex="4" class="form-control btn btn-primary" value="Accept">
-                  <br>
-                  <br>
-                  <input type="button" name="reject" id="reject" tabindex="4" class="form-control btn btn-primary" value="Reject">
-                  <br>
-                  <br>
-                  <a class="form-control btn btn-primary" href="adminFunction.php">Back</a>
+               <div class="col-md-12">
+                  <h2>Pending City Officials</h2>
+                  <div class="table-responsive">
+                     <table id="mytable" class="table table-bordred table-striped">
+                        <thead>
+                           <th></th>
+                           <th>Username</th>
+                           <th>City</th>
+                           <th>State</th>
+                           <th>Title</th>
+                        </thead>
+                        <tbody>
+                           <?php foreach($dataPoints as $dp) { ?>
+                              <tr>
+                                 <td><input name="selectedValue[]" value="<?php echo $dp['username']; ?>" type="checkbox" class="checkthis" /></td>
+                                 <td><?php echo $dp['username']; ?></td>
+                                 <td><?php echo $dp['city']; ?></td>
+                                 <td><?php echo $dp['state']; ?></td>
+                                 <td><?php echo $dp['title']; ?></td>
+                              </tr>
+                              <?php } ?>
+                        </tbody>
+                     </table>
+                  </div>
                </div>
             </div>
-         </div>
+            <div class="form-group">
+               <div class="row">
+                  <div class="col-sm-6 col-sm-offset-3">
+                     <input type="submit" name="accept" id="accept" tabindex="4" class="form-control btn btn-primary" value="accept">
+                     <br>
+                     <br>
+                     <input type="submit" name="reject" id="reject" tabindex="4" class="form-control btn btn-primary" value="reject">
+                     <br>
+                     <br>
+                     <a class="form-control btn btn-primary" href="adminFunction.php">Back</a>
+                  </div>
+               </div>
+            </div>
+         </form>
       </div>
       <script type="text/javascript" src="../assets/js/jquery-3.2.1.min.js" ></script>
       <script type="text/javascript" src="../assets/js/bootstrap.min.js" ></script>
---
+++ public/updateCityOfficial.php
@@ -0,0 +1,43 @@
+<?php
+/* Attempt MySQL server connection. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+
+require_once 'connect.php';
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+ 
+
+// $location = $_POST['location_name'];
+$usernames = $_POST['selectedValue'];
+print_r($usernames);
+$accepted = "";
+
+if (isset($_POST['accept']) && $_POST['accept'] == "accept") {
+	$accepted = "1";
+} else if (isset($_POST['reject']) && $_POST['reject'] == "reject") {
+	$accepted = "0";
+} else {
+	$accepted = "null";
+}
+
+$sql = "";
+
+foreach ($usernames as $u) {
+	$sql .= "update city_official set Approved = $accepted where Username = '$u'; ";
+}
+// echo $sql;
+// die();
+
+$host  = $_SERVER['HTTP_HOST'];
+$uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+$scientist = 'pendingCityOfficial.php';
+if(mysqli_multi_query($link, $sql)){
+	mysqli_close($link);
+	header("Location: http://$host$uri/$scientist");
+}
+ 
+// Close connection
+?>
