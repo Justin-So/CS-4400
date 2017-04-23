f317d072b4fa8c72c67e56c2bcf35226aafa0406
Justin So <Justin So>
2017-04-23 16:18:36 -0400

    sdf

--- public/viewPOI.php
+++ public/viewPOI.php
@@ -1,3 +1,31 @@
+<?php
+   require_once 'connect.php';
+   // $link = mysqli_connect("localhost", "cs4400_74", "e_zTUL5w", "cs4400_74");
+   // // $link = mysqli_connect("localhost", "root", "", "cs4400_74");
+    
+   // Check connection
+   if($link === false){
+       die("ERROR: Could not connect. " . mysqli_connect_error());
+   }
+   
+   $sql = 'select * from POI';
+   $location = array();
+   $citys = array();
+   $states = array();
+   if($result = mysqli_query($link, $sql)) {
+     while ($row = mysqli_fetch_array($result)) 
+     {
+       array_push($location, $row['Location_Name']);
+       array_push($citys, $row['City']);
+       array_push($states, $row['State']);
+     }
+   }
+   
+   mysqli_close($link);
+   // print_r($citys);
+   // print_r($states);
+   
+   ?>
 <!DOCTYPE html>
 <html>
    <head>
@@ -13,64 +41,58 @@
                <div class="panel-body">
                   <div class="row">
                      <div class="col-lg-12">
-                     <h1 class="center-text">View POIs </h1>
+                        <h1 class="center-text">View POIs </h1>
                         <form id="login-form" action="" method="post" role="form" style="display: block;">
                            <div class="form-group row">
                               <label class="col-2 col-form-label">POI Location Name</label>
                               <div class="col-10">
-                                 <select class="form-control">
-                                    <option >A</option>
-                                    <option >B</option>
-                                    <option >C</option>
+                                 <select name="location" class="form-control">
+                                 <?php foreach($location as $s) {
+                                    echo '<option>' . $s . '</option>';
+                                    } ?>
                                  </select>
                               </div>
                            </div>
-
                            <div class="form-group row">
                               <label class="col-2 col-form-label">City</label>
                               <div class="col-10">
-                                 <select class="form-control">
-                                    <option >A</option>
-                                    <option >B</option>
-                                    <option >C</option>
+                                 <select name="city" class="form-control">
+                                 <?php foreach($citys as $s) {
+                                    echo '<option>' . $s . '</option>';
+                                    } ?>
                                  </select>
                               </div>
                            </div>
-
                            <div class="form-group row">
                               <label class="col-2 col-form-label">State</label>
                               <div class="col-10">
-                                 <select class="form-control">
-                                    <option >A</option>
-                                    <option >B</option>
-                                    <option >C</option>
+                                 <select name="state" class="form-control">
+                                 <?php foreach($states as $s) {
+                                    echo '<option>' . $s . '</option>';
+                                    } ?>
                                  </select>
                               </div>
                            </div>
-
                            <div class="form-group row">
                               <label class="col-2 col-form-label">Zip Code</label>
                               <div class="col-10">
                                  <input type="text" class="form-control" name="dataValue">
                               </div>
                            </div>
-
                            <div class="form-group row">
                               <label class="col-2 col-form-label">Flagged</label>
                               <div class="col-10">
                                  <input type="checkbox" name="dataValue" value="">
                               </div>
                            </div>
-
                            <div class="form-group row">
-                             <label class="col-2 col-form-label">Date flagged</label>
-                             <div class="col-10">
-                               <input type="date" name="dataReadingDatetimeFrom">
-                                to
-                               <input type="date" name="dataReadingDatetimeTo">
-                             </div>
+                              <label class="col-2 col-form-label">Date flagged</label>
+                              <div class="col-10">
+                                 <input type="date" name="dataReadingDatetimeFrom">
+                                 to
+                                 <input type="date" name="dataReadingDatetimeTo">
+                              </div>
                            </div>
-
                            <div class="form-group row">
                               <div class="col-sm-6">
                                  <a href="#" class="form-control btn btn-primary">Apply Filter</a>
@@ -79,7 +101,6 @@
                                  <a href="#" class="form-control btn btn-primary">Reset Filter</a>
                               </div>
                            </div>
-
                         </form>
                      </div>
                   </div>
@@ -88,31 +109,31 @@
          </div>
          <hr>
          <div class="row">
-           <div class="col-sm-10 col-sm-offset-1">
-             <div class="table-responsive">
-                <table id="mytable" class="table table-bordred table-striped">
-                   <thead>
-                      <th>Location name</th>
-                      <th>City</th>
-                      <th>State</th>
-                      <th>Zip code</th>
-                      <th>Flagged?</th>
-                      <th>Date Flagged</th>
-                   </thead>
-                   <tbody>
-                      <tr>
-                         <td>Blah balh</td>
-                         <td>Mohsin</td>
-                         <td>Irshad</td>
-                         <td>30024</td>
-                         <td>isometric.mohsin@gmail.com</td>
-                         <td>isometric.mohsin@gmail.com</td>
-                      </tr>
-                   </tbody>
-                </table>
-             </div>
-             <a class="form-control btn btn-primary" href="cityOfficialFunction.php">Back</a>
-           </div>
+            <div class="col-sm-10 col-sm-offset-1">
+               <div class="table-responsive">
+                  <table id="mytable" class="table table-bordred table-striped">
+                     <thead>
+                        <th>Location name</th>
+                        <th>City</th>
+                        <th>State</th>
+                        <th>Zip code</th>
+                        <th>Flagged?</th>
+                        <th>Date Flagged</th>
+                     </thead>
+                     <tbody>
+                        <tr>
+                           <td>Blah balh</td>
+                           <td>Mohsin</td>
+                           <td>Irshad</td>
+                           <td>30024</td>
+                           <td>isometric.mohsin@gmail.com</td>
+                           <td>isometric.mohsin@gmail.com</td>
+                        </tr>
+                     </tbody>
+                  </table>
+               </div>
+               <a class="form-control btn btn-primary" href="cityOfficialFunction.php">Back</a>
+            </div>
          </div>
       </div>
       <script type="text/javascript" src="../assets/js/jquery-3.2.1.min.js" ></script>
